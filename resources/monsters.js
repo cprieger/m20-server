f81fbc9481277c9d00edf57b2ef31c4e2faa35ad
LRieger13 <leastuart92@gmail.com>
2016-01-31 14:20:42 -0600

    Update monsters.js
    Monster list

--- resources/monsters.js
+++ resources/monsters.js
@@ -8,15 +8,63 @@ module.exports = {
     monsterList: [
         {
             id: 1,
-            name: 'Frankenstein',
-            description: 'Flings bolts of lightning, very frightening.',
-            HPAmount: 150
+            name: 'Zombie',
+            description: 'Overrun, more and more seem to be coming.',
+            HPAmount: 50
         },
         {
             id: 2,
-            name: 'Mummy',
-            description: 'Makes breakfast for zombies',
+            name: 'Werewolf',
+            description: 'Claw, has a chance to infect you if he rolls a 20.',
             HPAmount: 150
+        },
+         {
+            id: 3,
+            name: 'Vampire',
+            description: 'Blood Drain',
+            HPAmount: 100
+        },
+        {
+            id:4,
+            name: 'Mummy',
+            description: 'Summon guard for protection, each guard has 100hp.',
+            HPAmount: 150
+        },
+         {
+            id: 5,
+            name: 'Frankenstein',
+            description: 'Flings bolts of lightning, very frightening. Player is paralyzed and can't attack nextround. ,
+            HPAmount: 250
+        },
+        {
+            id: 6,
+            name: 'Basilisk',
+            description: 'Strike, Also can kill player instantly if roll a 20.',
+            HPAmount: 300
+        },
+         {
+            id: 7,
+            name: 'Golem',
+            description: 'Heavy hitter.',
+            HPAmount: 300
+        },
+        {
+            id: 8,
+            name: 'Sphinx',
+            description: 'Riddle me this. Get it wrong does damage.',
+            HPAmount: 350
+        },
+         {
+            id: 9,
+            name: 'Wraith',
+            description: 'Reduces roll by 3.',
+            HPAmount: 200
+        },
+        {
+            id: 10,
+            name: 'Windego',
+            description: 'Can possess you and turn yoou on your teammates.',
+            HPAmount: 400
         },
     ]
 };
