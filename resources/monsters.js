12584c698536e728a501da95d8431124c6cfab82
cprieger <christopherrieger@hotmail.com>
2016-02-01 20:16:07 -0600

    Made an update to monsters so they make a little more sense.

--- resources/monsters.js
+++ resources/monsters.js
@@ -10,61 +10,71 @@ module.exports = {
             id: 1,
             name: 'Zombie',
             description: 'Overrun, more and more seem to be coming.',
+            ability: 'On critical success spawn another zombie',
             HPAmount: 50
         },
         {
             id: 2,
             name: 'Werewolf',
-            description: 'Claw, has a chance to infect you if he rolls a 20.',
+            description: 'An angry ball of fur, teeth, and claws.',
+            ability: 'On critical success get an infection deals 3 damage per round.',
             HPAmount: 150
         },
-         {
+        {
             id: 3,
             name: 'Vampire',
-            description: 'Blood Drain',
+            description: 'A big buck toothed baddy',
+            ability: 'Each hit it lands sucks that much health to you and gains that much.'
             HPAmount: 100
         },
         {
-            id:4,
+            id: 4,
             name: 'Mummy',
-            description: 'Summon guard for protection, each guard has 100hp.',
+            description: 'Mummy likes making food for other zombies and cleaning up. Loves cursing.',
+            ability: 'On critical success summon guard for protection, each guard has 100hp. ',
             HPAmount: 150
         },
-         {
+        {
             id: 5,
             name: 'Frankenstein',
-            description: 'Flings bolts of lightning, very frightening. Player is paralyzed and cant attack next round. ',
-            HPAmount: 250
+            description: 'Flings bolts of lightning, very very frightening.',
+            ability: 'Player is paralyzed and cant attack next round. ',
+            HPAmount: 150
         },
         {
             id: 6,
             name: 'Basilisk',
-            description: 'Strike, Also can kill player instantly if roll a 20.',
-            HPAmount: 300
+            description: 'Do not worry this is just a huge snake with a really poor attitude. Nothing to stress about.',
+            ability: 'Critical success means a player is incapacitated for 3 rounds.',
+            HPAmount: 200
         },
-         {
+        {
             id: 7,
             name: 'Golem',
-            description: 'Heavy hitter.',
-            HPAmount: 300
+            description: 'Likes rocks, but not rolls. Stone cold.',
+            ability: 'Critical Success means target player incapacitated.',
+            HPAmount: 200
         },
         {
             id: 8,
             name: 'Sphinx',
-            description: 'Riddle me this. Get it wrong does damage.',
-            HPAmount: 350
+            description: 'Riddles you with random riddles. Probably the most irritatingly smug monster in the mix.',
+            ability: 'Pick a player to find a riddle. If you fail take 20 damage. If you succeed gain 50xp and Sphinx disappears.',
+            HPAmount: 1
         },
-         {
+        {
             id: 9,
             name: 'Wraith',
-            description: 'Reduces roll by 3.',
+            description: 'See through but not walk through. Seems to be upset about something. Probably just hungry.',
+            ability: 'Reduces damage roll by 3.',
             HPAmount: 200
         },
         {
             id: 10,
             name: 'Windego',
-            description: 'Can possess you and turn you on your teammates.',
-            HPAmount: 400
+            description: 'A poor mortal who was possessed by this ravenous spirit.',
+            ability: 'Critical success possesses you and turns you against teammates.',
+            HPAmount: 300
         },
     ]
 };
